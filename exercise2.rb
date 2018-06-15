608c3e81f622b3c640018c3727fd0113d9f05ad7
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-14 21:39:56 -0400

    2.3, 2.4, 2.5 solved

--- exercise2.rb
+++ exercise2.rb
@@ -2,8 +2,20 @@ nombres = ["Violeta", "Andino", "Clemente", "Javiera", "Paula", "Pia", "Ray"]
 
 b1 = nombres.select {|x| x.length<=5}
 
-b2 = nombres.map {|x| x.downcase! }
+b2 = nombres.map {|x| x.downcase }
+
+b3 = nombres.select{|x| x.chr.eql?('P') } #/P./.match(x) 
+
+b4 = nombres.map {|x| x.length}
+
+b5 = nombres.map {|x| x.gsub(/[aeiou]/, '') }
+
 print b1
 puts ""
 print b2
 puts ""
+print b3
+puts ""
+print b4
+puts ""
+print b5

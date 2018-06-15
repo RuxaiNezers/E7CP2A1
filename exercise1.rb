cd9893f32eb6d2e1a6d723d21987821f37c9b963
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-14 21:05:03 -0400

    1.3, 1.4, 1.5, 1.6 solved

--- exercise1.rb
+++ exercise1.rb
@@ -1,12 +1,25 @@
- a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
 
- b = a.map { |x| x + 1 }
+b1 = a.map { |x| x + 1 }
 
- b2 = a.map { |x| x.to_f}
+b2 = a.map { |x| x.to_f }
 
- print b
- puts ""
- print b2
+b3 = a.map { |x| x.to_s } 
 
+b4 = a.reject { |x| x < 5}
 
+b5 = a.select { |x| x < 5}
 
+b6 = a.inject { |sum, value| sum + value}
+
+print b1
+puts " "
+print b2
+puts " "
+print b3
+puts " "
+print b4
+puts " "
+print b5
+puts " "
+print b6
